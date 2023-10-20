023062b84fbb23a7d986ac621cc8393e66c76ba0
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-10-20 12:32:38 -0700

    Script that reads stdin line by line and computes metrics

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" script that reads stdin line by line and computes metrics"""
+import re
+import sys
+
+
+input_format = r'^' \
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - ' \
+    r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] ' \
+    r'"GET /projects/260 HTTP/1.1" ' \
+    r'\d{3} \d+$'
+
+status_codes = (200, 301, 400, 401, 403, 404, 405, 500)
+
+
+def print_stats(lines):
+    """
+    Function that reads stdin line by line and computes metrics
+
+    Args:
+    lines(str): lines representing the stdin
+
+    Returns:
+    After every 10 lines and/or a keyboard interruption (CTRL + C),
+    prints the following statistics:
+        Total file size: File size: <total size>
+        Number of lines by status code: <status code>: <number>
+        status codes should be printed in ascending order
+
+    """
+
+    line_count = 1
+    status_count = 0
+    total_size = []
+    all_status_codes = []
+    try:
+        for line in lines:
+            if re.match(input_format, line):
+                parts = line.split(" ")
+                file_size = parts[-1]
+                total_size.append(int(file_size))
+                status_code = parts[-2]
+                all_status_codes.append(int(status_code))
+            if line_count % 10 == 0:
+                print("File size:", sum(total_size))
+                for i in status_codes:
+                    status_count = all_status_codes.count(i)
+                    if status_count > 0:
+                        print("{:d}: {:d}".format(i, status_count))
+            line_count += 1
+    except KeyboardInterrupt:
+        print("File size:", sum(total_size))
+        for i in status_codes:
+            status_count = all_status_codes.count(i)
+            if status_count > 0:
+                print("{:d}: {:d}".format(i, status_count))
+
+
+print_stats(sys.stdin)
