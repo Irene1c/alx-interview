6f5ea996c0f23b0259bc978aab64f17344a30999
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-22 01:35:59 -0800

    optimized solution for different scenarios and edge cases

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,14 +5,14 @@
 from typing import List
 
 
-def makeChange(coins: List[int], total: int):
+def makeChange(coins: List[int], total: int) -> int:
     """
     Determining the fewest number of coins needed to meet a given amount
     The value of a coin will always be an integer greater than 0
 
     Args:
-    coins: list
-    total: int
+    coins (List[int]): A list of coin values
+    total (int): The target total amount.
 
     Returns:
     If total is 0 or less, return 0
@@ -26,29 +26,39 @@ def makeChange(coins: List[int], total: int):
     -1
     >>> print(makeChange([1, 2, 3], -4))
     0
+    >>> print(makeChange([1, 2, 5], 11))
+    3
+    >>> print(makeChange([1, 2, 5], 8))
+    3
+    >>> print(makeChange([2, 4, 6], 10))
+    2
+    >>> print(makeChange([], 10))
+    -1
 
     """
-
-    num = 0
-    n = len(coins) - 1
-    coins.sort(reverse=True)
 
     if total <= 0:
         return 0
 
-    for i in coins:
-        if total % i == 0:
-            num += total // i
-            break
-        else:
-            total = total - i
-            if total < 0:
-                return -1
-            elif i == coins[n] and total != 0:
-                return -1
-            num += 1
+    if coins == []:
+        return -1
 
-    return num
+    num_coins = 0
+    coins.sort(reverse=True)
+
+    for val in coins:
+        if val <= 0:
+            return -1
+
+        num_coins += total // val
+        total = total % val
+        if total == 0:
+            break
+
+    if total == 0:
+        return num_coins
+    else:
+        return -1
 
 
 if __name__ == "__main__":
